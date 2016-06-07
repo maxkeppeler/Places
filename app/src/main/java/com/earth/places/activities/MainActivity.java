ce52f07587687a262bbc41a9123701d1562cb383
Maximilian Keppeler <maximilian.keppeler@gmail.com>
2016-06-07 15:38:04 +0200

    Optimizations regarding the drawer and drawerHeader method.

--- app/src/main/java/com/earth/places/activities/MainActivity.java
+++ app/src/main/java/com/earth/places/activities/MainActivity.java
@@ -2,6 +2,7 @@ package com.earth.places.activities;
 
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.TabLayout;
@@ -12,13 +13,31 @@ import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.Priority;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
+import com.earth.places.R;
+import com.earth.places.fragment.DrawerAbout;
+import com.earth.places.fragment.DrawerPlacesTabs;
+import com.earth.places.fragment.FragmentBookmarks;
 import com.earth.places.fragment.FragmentDisasters;
 import com.earth.places.fragment.FragmentGoodActs;
+import com.earth.places.fragment.FragmentPlaces;
+import com.earth.places.models.Disasters;
+import com.earth.places.models.GoodActs;
+import com.earth.places.models.Places;
+import com.earth.places.utilities.Constants;
+import com.earth.places.utilities.Dialogs;
+import com.earth.places.utilities.FilterLogic;
+import com.earth.places.utilities.Preferences;
+import com.mikepenz.iconics.IconicsDrawable;
 import com.mikepenz.materialdrawer.AccountHeader;
 import com.mikepenz.materialdrawer.AccountHeaderBuilder;
 import com.mikepenz.materialdrawer.Drawer;
@@ -29,20 +48,10 @@ import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
 import com.mikepenz.materialdrawer.model.SectionDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.Nameable;
-import com.earth.places.R;
-import com.earth.places.fragment.DrawerAbout;
-import com.earth.places.fragment.DrawerPlacesTabs;
-import com.earth.places.fragment.FragmentBookmarks;
-import com.earth.places.fragment.FragmentPlaces;
-import com.earth.places.models.Disasters;
-import com.earth.places.models.GoodActs;
-import com.earth.places.models.Places;
-import com.earth.places.utilities.Constants;
-import com.earth.places.utilities.Dialogs;
-import com.earth.places.utilities.FilterLogic;
-import com.earth.places.utilities.Preferences;
 
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import butterknife.Bind;
 
@@ -58,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private AccountHeader drawerHeader;
     private String[] drawerHeaderURLS;
-    public static int drawerIndex;
+    public static int index;
     @Bind(R.id.appBarLayout)
     AppBarLayout appBarLayout;
 
@@ -90,7 +99,8 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.addTab(tab1);
         tabLayout.addTab(tab2);
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
-        tabLayout.setSelectedTabIndicatorColor(ContextCompat.getColor(context, R.color.white));
+        tabLayout.setSelectedTabIndicatorColor(ContextCompat.getColor(context, R.color.tabIndicator));
+        tabLayout.setTabTextColors(ContextCompat.getColor(context, R.color.tabTextNotSelected), ContextCompat.getColor(context, R.color.tabTextSelected));
 
 //        TODO: Check if app was updated and had then the first start
 
@@ -103,6 +113,7 @@ public class MainActivity extends AppCompatActivity {
 
         drawerHeader = new AccountHeaderBuilder().withActivity(this)
                 .withSelectionFirstLine("Beautiful Places").withSelectionSecondLine("on our Earth")
+                .withHeightDp(250)
                 .build();
         drawerHeader();
 
@@ -110,7 +121,7 @@ public class MainActivity extends AppCompatActivity {
                 .withActivity(this)
                 .withAccountHeader(drawerHeader)
                 .withSavedInstance(savedInstanceState)
-                .withSelectedItem(0)
+                .withSelectedItem(Constants.ID_DRAWER_PLACES)
                 .withToolbar(toolbar)
                 .addDrawerItems(
                         new PrimaryDrawerItem().withName(Constants.DRAWER_PLACES).withIcon(Icon.gmd_terrain).withIdentifier(Constants.ID_DRAWER_PLACES).withBadgeStyle(new BadgeStyle()),
@@ -133,70 +144,61 @@ public class MainActivity extends AppCompatActivity {
                                 Intent intent = null;
                                 DrawerLayout drawerLayout = drawer.getDrawerLayout();
 
-                                drawerIndex = (int) drawerItem.getIdentifier();
+                                index = (int) drawerItem.getIdentifier();
 
-                                switch (drawerIndex) {
+                                if (index == Constants.ID_DRAWER_PLACES) {
+                                    fragment = new DrawerPlacesTabs();
+                                    setTabTexts(Constants.TAB_DISASTERS, Constants.TAB_GOOD_ACTS);
 
-                                    case 0: fragment = new DrawerPlacesTabs();
-//                                        setTabTexts(Constants.TAB_DISASTERS, Constants.TAB_GOOD_ACTS);
-
-                                        if (Places.getPlacesList() != null && FragmentBookmarks.getBookmarks() != null)
+                                    if (Places.getPlacesList() != null && FragmentBookmarks.getBookmarks() != null)
                                         updateTabTexts(0, Places.getPlacesList().size(), FragmentBookmarks.getBookmarks().size());
-
-                                        break;
-
-                                    case 1: fragment = new DrawerPlacesTabs();
-//                                        setTabTexts(Constants.TAB_DISASTERS, Constants.TAB_GOOD_ACTS);
-
-                                        if (Disasters.getDisastersList() != null && GoodActs.getGoodActsList() != null)
-                                            updateTabTexts(1, Disasters.getDisastersList().size(), GoodActs.getGoodActsList().size());
-
-                                        break;
-
-                                    case 2: fragment = new DrawerPlacesTabs();
-                                        setTabTexts(Constants.TAB_PEOPLE, Constants.TAB_WEBSITES);
-                                        break;
-
-                                    case 3: fragment = new DrawerAbout();
-                                        break;
-
-                                    case 4: intent = new Intent(context, Settings.class);
-                                        break;
-
-                                    default: fragment = new FragmentPlaces();
                                 }
 
+                                else if (index == Constants.ID_DRAWER_NATURE) {
+                                    fragment = new DrawerPlacesTabs();
+                                    setTabTexts(Constants.TAB_DISASTERS, Constants.TAB_GOOD_ACTS);
+
+                                    if (Disasters.getDisastersList() != null && GoodActs.getGoodActsList() != null)
+                                        updateTabTexts(1, Disasters.getDisastersList().size(), GoodActs.getGoodActsList().size());
+                                }
+
+                                else if (index == Constants.ID_DRAWER_HALL) {
+                                    fragment = new DrawerPlacesTabs();
+                                    setTabTexts(Constants.TAB_PEOPLE, Constants.TAB_WEBSITES);
+                                }
+
+                                else if (index == Constants.ID_DRAWER_ABOUT)
+                                    fragment = new DrawerAbout();
+
+                                else if (index == Constants.ID_DRAWER_SETTINGS)
+                                    intent = new Intent(context, Settings.class);
 
 
+                                // Show fragment or start activity
                                 if (fragment != null) {
                                     fragment.setRetainInstance(true);
                                     transaction.replace(R.id.container, fragment);
                                     transaction.commit();
-                                    toolbar.setTitle(toolbarTitle(drawerIndex));
+                                    toolbar.setTitle(toolbarTitle(index));
 
                                     // Expand AppBarLayout (Scroll layout flags hides components)
-                                    if (appBarLayout != null)
-                                        appBarLayout.setExpanded(true);
+                                    if (appBarLayout != null) appBarLayout.setExpanded(true);
 
                                 } else if (intent != null) {
                                     startActivity(intent);
                                     drawer.setSelection(Constants.ID_DRAWER_PLACES);
                                 }
 
+                                // Delete all filters when changing fragment
+                                if (drawerFilter != null) drawerFilter.setSelection(Constants.NO_SELECTION);
 
+                                // Show Tablayout just in some specific positions
+                                if (index == Constants.ID_DRAWER_PLACES || index == Constants.ID_DRAWER_NATURE || index == Constants.ID_DRAWER_HALL) tabLayout.setVisibility(View.VISIBLE);
+                                else tabLayout.setVisibility(View.GONE);
 
-                                if (drawerFilter != null)
-                                    drawerFilter.setSelection(Constants.NO_SELECTION);
-
-                                if (drawerIndex > 2)
-                                    tabLayout.setVisibility(View.GONE);
-                                else tabLayout.setVisibility(View.VISIBLE);
-
-                                if (drawerIndex > 0)
-                                    drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED, Gravity.RIGHT);
-
-                                else
-                                    drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNDEFINED, Gravity.RIGHT);
+                                // Deactivate the filter drawer when user is not in the places fragment
+                                if (index != Constants.ID_DRAWER_PLACES) drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED, Gravity.RIGHT);
+                                else drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNDEFINED, Gravity.RIGHT);
 
                             }
                         }
@@ -355,9 +357,8 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * Pick random another image for the drawer header
-     * @return drawerHeader
      */
-    public AccountHeader drawerHeader() {
+    public void drawerHeader() {
 
         final ImageView cover = drawerHeader.getHeaderBackgroundView();
         Random random = new Random();
@@ -365,12 +366,23 @@ public class MainActivity extends AppCompatActivity {
 
         Glide.with(context)
                 .load(randomURL)
-                .crossFade()
-                .override(1012, 788)
+                .crossFade(50)
+                .priority(Priority.IMMEDIATE)
+                .override(1152, 648)
                 .centerCrop()
-                .into(cover);
+                .listener(new RequestListener<String, GlideDrawable>() {
+                    @Override
+                    public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
+                        drawerHeader();
+                        return false;
+                    }
 
-        return drawerHeader;
+                    @Override
+                    public boolean onResourceReady(GlideDrawable resource, String model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
+                        return false;
+                    }
+                })
+                .into(cover);
     }
 
     @Override
